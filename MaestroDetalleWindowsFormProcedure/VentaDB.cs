c3d1ce54710409343017bfc4ccd7e3d036f5ed06
AxelJaBo <axel.910921@gmail.com>
2024-01-28 19:19:03 -0800

    Configure Database Connection

---
+++ MaestroDetalleWindowsFormProcedure/VentaDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaestroDetalleWindowsFormProcedure
+{
+    public class VentaDB
+    {
+        private string connectionString =
+            "Data Source=localhost; initial Catalog=MaestroDetalleProcedimiento;" +
+            "User=DESKTOP-KA53HTR;Password=";
+
+        public void Add(string Cliente)
+        {
+            string query = "insert into venta(cliente) values(@cliente)";
+            using (SqlConnection connection = new SqlConnection())
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@cliente", Cliente);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
